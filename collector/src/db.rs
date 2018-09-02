016b99e15e63028ea52b391d57255c114e3850d5
zeljic <zeljic@gmail.com>
2018-09-02 20:45:20 +0200

    Diesel models and connection pool

---
+++ collector/src/db.rs
@@ -0,0 +1,46 @@
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+use diesel::sqlite::SqliteConnection;
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Outcome, Request, State};
+use std::ops::Deref;
+
+use dotenv::dotenv;
+use std::env;
+
+pub type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
+
+pub fn init_pool() -> SqlitePool {
+	dotenv().ok();
+
+	println!("init_pool");
+
+	let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+	let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+
+	Pool::new(manager).expect("Database Poll is not created")
+}
+
+pub struct DbConn(pub PooledConnection<ConnectionManager<SqliteConnection>>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+	type Error = ();
+
+	fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+		let pool = request.guard::<State<SqlitePool>>()?;
+
+		match pool.get() {
+			Ok(conn) => Outcome::Success(DbConn(conn)),
+			Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+		}
+	}
+}
+
+impl Deref for DbConn {
+	type Target = SqliteConnection;
+
+	fn deref(&self) -> &Self::Target {
+		&self.0
+	}
+}
