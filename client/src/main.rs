bbeb072b4b5433c0549dc092375a6f3da968f01c
zeljic <zeljic@gmail.com>
2018-09-10 23:14:59 +0200

    How to read machine-id and do sha256 on it, also use lazy_static

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,43 @@
+extern crate crypto;
+#[macro_use]
+extern crate lazy_static;
+
+#[cfg(linux)]
+#[inline(always)]
+fn get_machine_id() -> String {
+	use std::fs::File;
+	use std::io::prelude::*;
+
+	let mut f = File::open("/etc/machine-id").unwrap();
+	let mut content: String = String::new();
+
+	f.read_to_string(&mut content).unwrap();
+
+	content
+}
+
+#[cfg(not(linux))]
+#[inline(always)]
+fn get_machine_id() -> String {
+	"windows_machine_id".to_string()
+}
+
+#[inline(always)]
+fn get_uuid() -> String {
+	use crypto::digest::Digest;
+	use crypto::sha2::Sha256;
+
+	let mut sha = Sha256::new();
+	sha.input_str(&get_machine_id());
+
+	sha.result_str()
+}
+
+lazy_static! {
+	static ref MACHINE_ID: String = get_machine_id();
+	static ref UUID: String = get_uuid();
+}
+
 fn main() {
-	println!("Zdravo, client!");
+	println!("MACHINE_ID: {}; UUID: {};", *MACHINE_ID, *UUID);
 }
