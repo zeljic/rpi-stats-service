c54d515311a5550b5c69af3611c38a2c368a6c67
zeljic <zeljic@gmail.com>
2018-09-19 07:30:39 +0200

    create log basic implementation

--- collector/src/db/models/instance.rs
+++ collector/src/db/models/instance.rs
@@ -1,6 +1,13 @@
 use db::models::CRUD;
+use db::pool::PoolWrapper;
+use rocket::http::Status;
+use rocket::request;
+use rocket::request::FromRequest;
+use rocket::Outcome;
+use rocket::Request;
 use rusqlite::Connection;
 use rusqlite::Error;
+use rusqlite::Row;
 use rusqlite::Statement;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -22,14 +29,7 @@ impl CRUD for Instance {
 		let mut rows = statement.query(&[&id]).expect("Unable to execute query");
 
 		if let Some(result_row) = rows.next() {
-			let row = result_row.expect("Unable to get row");
-
-			Some(Instance {
-				id: row.get("id"),
-				uuid: row.get("uuid"),
-				name: row.get("name"),
-				enabled: row.get("enabled"),
-			})
+			Some(Instance::from(result_row.expect("Unable to get row")))
 		} else {
 			None
 		}
@@ -41,5 +41,53 @@ impl CRUD for Instance {
 			.expect("Unable to prepare statement");
 
 		statement.execute(&[&self.uuid, &self.name])
+	}
+}
+
+impl Instance {
+	fn get_by_uuid(conn: &Connection, uuid: &str) -> Option<Self> {
+		let mut statement: Statement = conn
+			.prepare("select * from `instance` where uuid = ?;")
+			.expect("Unable to prepare statement");
+
+		let mut rows = statement.query(&[&uuid]).expect("Unable to execute query");
+
+		if let Some(result_row) = rows.next() {
+			Some(Instance::from(result_row.expect("Unable to get row")))
+		} else {
+			None
+		}
+	}
+}
+
+impl<'a, 'stmt> From<Row<'a, 'stmt>> for Instance {
+	fn from(row: Row) -> Self {
+		Instance {
+			id: row.get("id"),
+			uuid: row.get("uuid"),
+			name: row.get("name"),
+			enabled: row.get("enabled"),
+		}
+	}
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for Instance {
+	type Error = ();
+
+	fn from_request(request: &'a Request<'r>) -> request::Outcome<Instance, ()> {
+		match request.headers().get_one("X-Instance-UUID") {
+			Some(uuid) => {
+				let pool_wrapper = PoolWrapper::from_request(request);
+
+				match Instance::get_by_uuid(
+					&*(pool_wrapper.expect("Unable to get connection pool")),
+					uuid,
+				) {
+					Some(instance) => Outcome::Success(instance),
+					None => Outcome::Failure((Status::BadRequest, ())),
+				}
+			}
+			None => Outcome::Failure((Status::BadRequest, ())),
+		}
 	}
 }
--- collector/src/main.rs
+++ collector/src/main.rs
@@ -1,4 +1,4 @@
-#![feature(plugin)]
+#![feature(plugin, decl_macro)]
 #![plugin(rocket_codegen)]
 
 extern crate dotenv;
@@ -8,14 +8,18 @@ extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 extern crate rusqlite;
-
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde;
 extern crate serde_json;
 
+use db::models::instance::Instance;
+use db::models::log::Log;
+use db::models::CRUD;
 use db::pool::init_pool;
+use db::pool::PoolWrapper;
 use rocket_contrib::{Json, Value};
+use std::error::Error;
 
 mod db;
 
@@ -28,8 +32,31 @@ fn index() -> &'static str {
 fn error_404() -> Json<Value> {
 	Json(json!({
 		"status": false,
-		"reason": "Unable to handle this request"
+		"reason": "Handler Not Found: Unable to handle this request"
 	}))
+}
+
+#[catch(400)]
+fn error_400() -> Json<Value> {
+	Json(json!({
+		"status": false,
+		"reason": "Bad Request: Unable to handle this request"
+	}))
+}
+
+#[post("/", format = "application/json", data = "<json_log>")]
+fn log_create(instance: Instance, conn: PoolWrapper, json_log: Json<Log>) -> Json<Value> {
+	println!("{:?}", instance);
+
+	match json_log.into_inner().create(&*conn) {
+		Ok(_log) => Json(json!({
+				"status": true
+			})),
+		Err(e) => Json(json!({
+				"status": false,
+				"reason": e.description()
+			})),
+	}
 }
 
 fn main() {
@@ -38,9 +65,9 @@ fn main() {
 	rocket::ignite()
 		.manage(pool)
 		.mount("/", routes![index])
-		.mount("/api/log", routes![])
+		.mount("/api/log", routes![log_create])
 		.mount("/api/log_type", routes![])
 		.mount("/api/instance", routes![])
-		.catch(catchers![error_404])
+		.catch(catchers![error_400, error_404])
 		.launch();
 }
