55fd56138620ebdcf4498f1388fe709ad7dffdf5
zeljic <zeljic@gmail.com>
2018-12-29 19:54:40 +0100

    setup proxy in dev mode

--- web-ui/vue.config.js
+++ web-ui/vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
 	configureWebpack: config => {
 		config.resolve.alias['vue$'] = 'vue/dist/vue.esm.js';
+
+		config.devServer = config.devServer || {};
+
+		config.devServer.proxy = {
+			'/api': {
+				target: 'http://127.0.0.1'
+			}
+		};
 	},
 
 	productionSourceMap: false
