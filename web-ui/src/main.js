72522e3edec85b588d7a809aa40503dc74b41d0b
zeljic <zeljic@gmail.com>
2018-12-29 21:15:52 +0100

    load plugins

--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import './plugins/vuetify';
-import App from './layout/App.vue';
+import Layout from './layout/Layout.vue';
 import router from './router/router';
 import store from './store/store';
+import Http from './plugins/http';
+import User from './common/User';
 
 Vue.config.productionTip = false;
+
+Vue.use(new Http());
+Vue.use(new User(Vue, store));
 
 new Vue({
 	router,
 	store,
-	render: h => h(App)
+	render: h => h(Layout)
 }).$mount('#app');
