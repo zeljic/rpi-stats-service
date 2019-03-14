8bb25e359786f7d21f67cb60775cde0511ad1a72
zeljic <zeljic@gmail.com>
2019-03-15 00:11:17 +0100

    load custom styles

--- web-ui/src/plugins/vuetify.js
+++ web-ui/src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'vuetify/src/stylus/app.styl';
+import '../stylus/main.styl';
 
 Vue.use(Vuetify, {
 	iconfont: 'md',
