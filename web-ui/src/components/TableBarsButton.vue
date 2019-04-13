9f14d82671eba6deeb15bd61462ee8cb1382d4e5
zeljic <zeljic@gmail.com>
2019-04-13 23:19:06 +0200

    added table bars button

---
+++ web-ui/src/components/TableBarsButton.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-menu lazy offset-y transition="slide-x-transition">
+		<template v-slot:activator="{ on }">
+			<v-btn small icon flat v-on="on">
+				<v-icon small>fas fa-bars</v-icon>
+			</v-btn>
+		</template>
+		<slot></slot>
+	</v-menu>
+</template>
+
+<script>
+	export default {
+		name: 'table-bars-button',
+		data()
+		{
+			return {};
+		}
+	};
+</script>
