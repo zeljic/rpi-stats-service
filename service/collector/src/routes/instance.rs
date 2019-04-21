32c978c55646d16b20d6ee950422aecc17904376
zeljic <zeljic@gmail.com>
2019-04-21 21:47:41 +0200

    list instances

--- service/collector/src/routes/instance.rs
+++ service/collector/src/routes/instance.rs
@@ -1,24 +1,39 @@
 use crate::db::models::user::User;
 use rocket_contrib::json::JsonValue;
 
-use crate::db::models::user::UserModel;
-use crate::db::models::user_mesh::UserMeshModel;
-use crate::db::models::ModelAs;
+use crate::db::models::instance::{InstanceJson, InstanceModel};
+
 use crate::db::DatabaseConnection;
 use diesel::prelude::*;
+
+use diesel::sql_types::Integer;
 use rocket::Route;
+use std::error::Error;
 
 #[get("/", format = "application/json")]
-pub fn get(conn: DatabaseConnection, user: User) -> JsonValue {
-	let model: UserModel = user.as_model().as_ref().clone();
+pub fn list(conn: DatabaseConnection, user: User) -> Result<JsonValue, Box<dyn Error>> {
+	let list = diesel::sql_query(
+		r#"
+		select * from instance where id in (
+			select instance_id from mesh_instance where mesh_id in (
+				select mesh_id from user_mesh where	user_id = $1
+			)
+		);
+		"#,
+	)
+	.bind::<Integer, _>(user.get_id())
+	.get_results::<InstanceModel>(&conn.0)?
+	.into_iter()
+	.map(std::convert::Into::into)
+	.collect::<Vec<InstanceJson>>();
 
-	if let Ok(_list) = UserMeshModel::belonging_to(&model).load::<UserMeshModel>(&conn.0) {}
-
-	json!({
-		"status": false
-	})
+	Ok(json!({
+	"status": true,
+	"list": list,
+	"count": list.len()
+	}))
 }
 
 pub fn get_routes() -> Vec<Route> {
-	routes![get]
+	routes![list]
 }
