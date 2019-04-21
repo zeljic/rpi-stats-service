e5588821a2c0e85993c25df0abb70b7e051fcf1c
zeljic <zeljic@gmail.com>
2019-04-21 22:05:09 +0200

    simple meshes and instances tables

--- web-ui/src/views/instances/InstancesTable.vue
+++ web-ui/src/views/instances/InstancesTable.vue
@@ -1,21 +1,100 @@
 <template>
 	<container-view>
-		<toolbar>
-			Instances
-			<template slot="actions">
-				<v-btn flat>Do it!</v-btn>
-			</template>
-		</toolbar>
+
+		<v-layout row wrap>
+			<v-flex xs12>
+				<toolbar>
+					Instances
+
+					<template v-slot:actions>
+						<v-btn icon flat to="instances/create">
+							<v-icon small>fas fa-plus</v-icon>
+						</v-btn>
+
+						<v-btn icon flat @click="fetch">
+							<v-icon small>fas fa-sync</v-icon>
+						</v-btn>
+					</template>
+				</toolbar>
+			</v-flex>
+
+			<v-flex xs12>
+				<v-data-table
+					:headers="headers"
+					:items="items"
+					:loading="loading"
+					class="elevation-1"
+					hide-actions
+				>
+					<template v-slot:items="props">
+						<td class="w-10">{{props.item.id}}</td>
+						<td class="w-200">{{props.item.name}}</td>
+						<td class="w-200">{{props.item.uuid}}</td>
+						<td>{{props.item.description}}</td>
+						<td class="text-xs-center">
+							<v-icon small>fas {{props.item.enabled ? 'fa-check' : 'fa-clock'}}</v-icon>
+						</td>
+						<td class="w-10 pa-0 text-xs-center">
+							<TableBarButton>
+								<v-list dense>
+									<v-list-tile :to="'/instances/update/' + props.item.id">
+										<v-list-tile-avatar>
+											<v-icon small>fas fa-pencil-alt</v-icon>
+										</v-list-tile-avatar>
+										<v-list-tile-content>
+											<v-list-tile-title>Edit</v-list-tile-title>
+										</v-list-tile-content>
+									</v-list-tile>
+
+									<v-list-tile @click.stop="showDeleteFn(props.item)">
+										<v-list-tile-avatar>
+											<v-icon small>fas fa-trash-alt</v-icon>
+										</v-list-tile-avatar>
+										<v-list-tile-content>
+											<v-list-tile-title>Delete</v-list-tile-title>
+										</v-list-tile-content>
+									</v-list-tile>
+								</v-list>
+							</TableBarButton>
+						</td>
+					</template>
+				</v-data-table>
+			</v-flex>
+		</v-layout>
+
+		<delete-dialog
+			:show.sync="deleteDialog.show"
+			:url="`/api/instance/${this.deleteDialog.id}`"
+			close-on-success
+			@success="deleted"
+		></delete-dialog>
 	</container-view>
 </template>
 
 <script>
+	import TableBarButton from '../../components/TableBarsButton';
 
 	export default {
-		name: 'instances-table-view',
+		name: 'instance-table',
+		components: {TableBarButton},
 		data()
 		{
-			return {};
+			return {
+				loading: false,
+				deleteDialog: {
+					show: false,
+					id: null
+				},
+				headers: [
+					{text: 'ID', value: 'id'},
+					{text: 'Name', value: 'name'},
+					{text: 'UUID', value: 'uuid'},
+					{text: 'Description', value: 'description'},
+					{text: 'Status', value: 'enabled', align: 'center', class: 'w-50', sortable: false},
+					{text: '', value: 'id', 'class': 'pa-0 ma-0', sortable: false}
+				],
+				items: []
+			};
 		},
 		created()
 		{
@@ -24,16 +103,29 @@
 		methods: {
 			fetch()
 			{
+				this.loading = false;
+
+				this.items = [];
+
 				this.$http({
-					url: '/api/mesh',
+					url: '/api/instance',
 					method: 'get'
-				}).then(() =>
+				}).then(({status, data}) =>
 				{
-
-				}).then(() =>
-				{
-
-				});
+					if (status === 200)
+					{
+						this.items = data.list;
+					}
+				}).finally(() => this.loading = false);
+			},
+			showDeleteFn(item)
+			{
+				this.deleteDialog.show = true;
+				this.deleteDialog.id = item.id;
+			},
+			deleted()
+			{
+				this.fetch();
 			}
 		}
 	};
--- web-ui/src/views/meshes/MeshesTable.vue
+++ web-ui/src/views/meshes/MeshesTable.vue
@@ -1,23 +1,130 @@
 <template>
 	<container-view>
-		<toolbar>Meshes</toolbar>
+
+		<v-layout row wrap>
+			<v-flex xs12>
+				<toolbar>
+					Meshes
+
+					<template v-slot:actions>
+						<v-btn icon flat to="meshes/create">
+							<v-icon small>fas fa-plus</v-icon>
+						</v-btn>
+
+						<v-btn icon flat @click="fetch">
+							<v-icon small>fas fa-sync</v-icon>
+						</v-btn>
+					</template>
+				</toolbar>
+			</v-flex>
+
+			<v-flex xs12>
+				<v-data-table
+					:headers="headers"
+					:items="items"
+					:loading="loading"
+					class="elevation-1"
+					hide-actions
+				>
+					<template v-slot:items="props">
+						<td class="w-10">{{props.item.id}}</td>
+						<td class="w-200">{{props.item.name}}</td>
+						<td>{{props.item.description}}</td>
+						<td class="text-xs-center">
+							<v-icon small>fas {{props.item.enabled ? 'fa-check' : 'fa-clock'}}</v-icon>
+						</td>
+						<td class="w-10 pa-0 text-xs-center">
+							<TableBarButton>
+								<v-list dense>
+									<v-list-tile :to="'/meshes/update/' + props.item.id">
+										<v-list-tile-avatar>
+											<v-icon small>fas fa-pencil-alt</v-icon>
+										</v-list-tile-avatar>
+										<v-list-tile-content>
+											<v-list-tile-title>Edit</v-list-tile-title>
+										</v-list-tile-content>
+									</v-list-tile>
+
+									<v-list-tile @click.stop="showDeleteFn(props.item)">
+										<v-list-tile-avatar>
+											<v-icon small>fas fa-trash-alt</v-icon>
+										</v-list-tile-avatar>
+										<v-list-tile-content>
+											<v-list-tile-title>Delete</v-list-tile-title>
+										</v-list-tile-content>
+									</v-list-tile>
+								</v-list>
+							</TableBarButton>
+						</td>
+					</template>
+				</v-data-table>
+			</v-flex>
+		</v-layout>
+
+		<delete-dialog
+			:show.sync="deleteDialog.show"
+			:url="`/api/mesh/${this.deleteDialog.id}`"
+			close-on-success
+			@success="deleted"
+		></delete-dialog>
 	</container-view>
 </template>
 
 <script>
+	import TableBarButton from '../../components/TableBarsButton';
+
 	export default {
-		name: '',
+		name: 'meshes-table',
+		components: {TableBarButton},
 		data()
 		{
-			return {};
+			return {
+				loading: false,
+				deleteDialog: {
+					show: false,
+					id: null
+				},
+				headers: [
+					{text: 'ID', value: 'id'},
+					{text: 'Name', value: 'name'},
+					{text: 'Description', value: 'description'},
+					{text: 'Status', value: 'enabled', align: 'center', class: 'w-50', sortable: false},
+					{text: '', value: 'id', 'class': 'pa-0 ma-0', sortable: false}
+				],
+				items: []
+			};
 		},
 		created()
 		{
+			this.fetch();
 		},
-		methods: {}
+		methods: {
+			fetch()
+			{
+				this.loading = false;
+
+				this.items = [];
+
+				this.$http({
+					url: '/api/mesh',
+					method: 'get'
+				}).then(({status, data}) =>
+				{
+					if (status === 200)
+					{
+						this.items = data.list;
+					}
+				}).finally(() => this.loading = false);
+			},
+			showDeleteFn(item)
+			{
+				this.deleteDialog.show = true;
+				this.deleteDialog.id = item.id;
+			},
+			deleted()
+			{
+				this.fetch();
+			}
+		}
 	};
 </script>
-
-<style scoped>
-
-</style>
