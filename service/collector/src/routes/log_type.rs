95174577b648bfcd65914cd43dacdeaa3318b15f
zeljic <zeljic@gmail.com>
2019-04-24 01:19:21 +0200

    start working on log_type route optimization

--- service/collector/src/routes/log_type.rs
+++ service/collector/src/routes/log_type.rs
@@ -16,24 +16,33 @@ use crate::db::models::schema::log_type::dsl as log_type_dsl;
 use std::error::Error;
 
 #[get("/", format = "application/json")]
-pub fn list(conn: DatabaseConnection, user: User) -> JsonValue {
-	let user_model = user.as_model().as_ref().clone();
+pub fn list(conn: DatabaseConnection, user: User) -> Result<JsonValue, Box<dyn Error>> {
+	let list = log_type_dsl::log_type
+		.filter(log_type_dsl::user_id.eq(user.get_id()))
+		.get_results::<LogTypeModel>(&conn.0)?
+		.into_iter()
+		.map(std::convert::Into::into)
+		.collect::<Vec<LogTypeJson>>();
 
-	if let Ok(list) = LogTypeModel::belonging_to(&user_model).load::<LogTypeModel>(&conn.0) {
-		let list = list
-			.into_iter()
-			.map(std::convert::Into::into)
-			.collect::<Vec<LogTypeJson>>();
+	Ok(json!({
+	"status": true,
+	"list": list,
+	"count": list.len()
+	}))
+}
 
-		return json!({
-			"status": true,
-			"list": list
-		});
-	}
+#[get("/<id>", format = "application/json")]
+pub fn get(conn: DatabaseConnection, user: User, id: i32) -> Result<JsonValue, Box<dyn Error>> {
+	let item: LogTypeJson = log_type_dsl::log_type
+		.filter(log_type_dsl::id.eq(id))
+		.filter(log_type_dsl::user_id.eq(user.get_id()))
+		.first::<LogTypeModel>(&conn.0)?
+		.into();
 
-	json!({
-		"status": false
-	})
+	Ok(json!({
+		"status": true,
+		"item": item
+	}))
 }
 
 #[post("/", format = "application/json", data = "<create_request>")]
@@ -42,11 +51,9 @@ pub fn create(
 	user: User,
 	create_request: Json<LogTypeJson>,
 ) -> JsonValue {
-	let mut create_request = create_request.clone();
+	let mut create_request: LogTypeJson = create_request.into_inner();
 
-	let user_model = user.as_model().as_ref().clone();
-
-	create_request.user_id.get_or_insert(user_model.id);
+	create_request.user_id.get_or_insert(user.get_id());
 
 	match diesel::insert_into(log_type_dsl::log_type)
 		.values(&create_request)
@@ -61,60 +68,32 @@ pub fn create(
 	}
 }
 
-#[get("/<id>", format = "application/json")]
-pub fn get(conn: DatabaseConnection, user: User, id: i32) -> JsonValue {
-	if let Ok(item) = log_type_dsl::log_type
-		.filter(log_type_dsl::id.eq(id))
-		.filter(log_type_dsl::user_id.eq(user.as_model().id))
-		.first::<LogTypeModel>(&conn.0)
-	{
-		return json!({
-			"status": true,
-			"item": LogTypeJson::from(item)
-		});
-	}
-
-	json!({
-		"status": true
-	})
-}
-
-#[put("/", format = "application/json", data = "<update_request>")]
+#[put("/<id>", format = "application/json", data = "<update_request>")]
 pub fn update(
 	conn: DatabaseConnection,
 	user: User,
 	update_request: Json<LogTypeJson>,
-) -> Result<JsonValue, AsJsonError> {
+	id: i32,
+) -> Result<JsonValue, Box<dyn Error>> {
 	let update_request = &update_request.into_inner();
 
-	if let Ok(log_type) = LogType::from_json(&conn, update_request) {
-		if !log_type.is_user_id(&user) {
-			return Err(AsJsonError::new("Security issue"));
-		}
+	let log_type_model = LogType::new(&conn, id)?.as_model();
 
-		let log_type_model: LogTypeModel = log_type.as_model().as_ref().clone();
-
-		match diesel::update(&log_type_model)
-			.set(update_request)
-			.execute(&conn.0)
-		{
-			Ok(_) => {
-				return Ok(json!({
-					"status": true
-				}));
-			}
-			Err(_) => {
-				return Ok(json!({
-					"status": false
-				}));
-			}
-		}
+	if log_type_model.user_id != user.get_id() {
+		return Err(Box::new(AsJsonError::new("Security issue")));
 	}
 
+	let item: LogTypeJson = diesel::update(log_type_model.as_ref())
+		.set(update_request)
+		.get_result::<LogTypeModel>(&conn.0)?
+		.into();
+
 	Ok(json!({
-		"status": false
+		"status": true,
+		"item": item
 	}))
 }
+
 #[delete("/<id>")]
 pub fn delete(conn: DatabaseConnection, user: User, id: i32) -> Result<JsonValue, AsJsonError> {
 	let log_type = LogType::new(&conn, id as i32);
